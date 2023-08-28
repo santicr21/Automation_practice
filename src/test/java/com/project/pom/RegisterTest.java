c10fffb3a3a5011c09dbdedc9d5c75b85669e7ff
Santiago Caicedo Rojas <santiago.caicedo@globant.com>
2023-08-28 13:36:12 -0500

    Added: Test of register user

---
+++ src/test/java/com/project/pom/RegisterTest.java
@@ -0,0 +1,31 @@
+package com.project.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class RegisterTest {
+    private WebDriver driver;
+    RegisterPage registerPage;
+
+    @BeforeClass
+    public void setUp () {
+        registerPage = new RegisterPage(driver);
+        driver = registerPage.chromeDriverConnection();
+        registerPage.maximizeWindow();
+        registerPage.visit("https://demo.guru99.com/test/newtours/");
+    }
+
+    @AfterClass
+    public void tearDown () {
+        driver.quit();
+    }
+
+    @Test
+    public void test1 () throws InterruptedException {
+        registerPage.registerUser();
+        Assert.assertEquals(registerPage.registeredMessage(), "Note: Your user name is santicr.", "Register failed");
+    }
+}
