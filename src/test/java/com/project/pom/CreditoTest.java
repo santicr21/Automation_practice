c5340da48b622dbcaa2e5616c355a1ca431cb810
santicr <santicr@javerianacali.edu.co>
2023-08-28 20:44:42 -0500

    Added: Credito POM structure

---
+++ src/test/java/com/project/pom/CreditoTest.java
@@ -0,0 +1,31 @@
+package com.project.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class CreditoTest {
+    private WebDriver driver;
+    CreditoPage creditoPage;
+
+    @BeforeClass
+    public void setUp () {
+        creditoPage = new CreditoPage(driver);
+        driver = creditoPage.chromeDriverConnection();
+        creditoPage.maximizeWindow();
+        creditoPage.visit("https://www.avvillas.com.co/wps/portal/avvillas/banco/banca-personal/simulador-credito-libre-inversion/");
+    }
+
+    @AfterClass
+    public void tearDown () {
+        driver.quit();
+    }
+
+    @Test
+    public void testSimulateCredit () throws InterruptedException {
+        creditoPage.simulateCredit();
+        Assert.assertEquals(creditoPage.getAnswerMessage(), "En este momento estamos presentando fallas técnicas, por favor haz clic en \"calcular\" para enviar tu información nuevamente.", "Test failed");
+    }
+}
